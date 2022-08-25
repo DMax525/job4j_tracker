775d8515e65ad583237da070cb05e615f81e2243
dmax <dmax45@gmail.com>
2022-08-25 16:42:26 +0500

    3. Массивы и модели. [#174892]

---
+++ src/main/java/ru/job4j/pojo/Book.java
@@ -0,0 +1,20 @@
+package ru.job4j.pojo;
+
+public class Book {
+
+    private String title;
+    private int pages;
+
+    public Book(String title, int pages) {
+        this.title = title;
+        this.pages = pages;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+}
