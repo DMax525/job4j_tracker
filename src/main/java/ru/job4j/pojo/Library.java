ae0139305ce55fc644d6165c3185614486e2952c
dmax <dmax45@gmail.com>
2022-08-26 11:57:15 +0500

    3. Массивы и модели. [#174892 #336287]

--- src/main/java/ru/job4j/pojo/Library.java
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -6,16 +6,16 @@ public class Library {
         Book book1 = new Book("Book 1", 245);
         Book book2 = new Book("Book 2", 123);
         Book book3 = new Book("Book 3", 14);
-        Book CleanCode = new Book("Clean Code", 666);
+        Book cleanCode = new Book("Clean Code", 666);
         Book[] array = new Book[4];
         array[0] = book1;
         array[1] = book2;
         array[2] = book3;
-        array[3] = CleanCode;
+        array[3] = cleanCode;
         for (Book bk : array) {
             System.out.println(bk.getTitle() + ", " + bk.getPages());
         }
-        array[0] = CleanCode;
+        array[0] = cleanCode;
         array[3] = book1;
         for (Book bk : array) {
             if (bk.getTitle().equals("Clean Code")) {
