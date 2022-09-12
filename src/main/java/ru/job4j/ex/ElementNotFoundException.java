4f3bfb85c59fabbe2b7dbe0937f28c65a7a05ad6
dmax <dmax45@gmail.com>
2022-09-12 16:09:38 +0500

    0.4. Пользовательские исключения. [#219367 #342371]

--- src/main/java/ru/job4j/ex/ElementNotFoundException.java
+++ src/main/java/ru/job4j/ex/ElementNotFoundException.java
@@ -8,7 +8,6 @@ public class ElementNotFoundException extends Exception {
     public static void main(String[] args) {
         String[] array = {"Pipa", "Pupa"};
         String key = "Baba";
-        //indexOf(array, key);
         try {
             FindEl.indexOf(array, key);
         } catch (ElementNotFoundException e) {
